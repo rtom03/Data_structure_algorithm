f4cb0420632237d41e671e31a9eb1a3eba9efe66
zucm709 <zuckm709@gmail.com>
2024-06-09 18:20:38 +0100

    new algorithm added

---
+++ src/lib/goinnUp.index.js
@@ -0,0 +1,16 @@
+function GoinUp(n){
+    console.log('Our Rate Is Going Up...')
+    for(let i = 1;i <= n ; i++){
+        console.log(i)
+    }
+    console.log('Your Rate is Going Down...')
+    for(let x = n - 0; x >= 0; x--){
+        console.log(x)
+    }
+    console.log(`your Rate Has Been Plumbered ${'You are Out'}`)
+}
+console.log(GoinUp(20))
+
+
+
+
