50d719dfc63f0ec18d30054a6b4d175162cc2ec3
zucm709 <zuckm709@gmail.com>
2024-07-06 18:32:06 +0100

    Dashboard v-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { routes } from '../config/route'
 import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
+import { MdDashboard } from 'react-icons/md'
 
 const Navbar = () => {
   
   return (
     <div className='ga-maamli-regular'>
-      <h2>Dashboard</h2>
+      <div className='flex flex-col justufy-center'>
+      <div className='flex flex-row gap-2 items-center pb-5'>
+        <MdDashboard />
+      <h2 className='header'>Dashboard</h2>
+      </div>
       <div className='tab-links active-link'>
         {routes.map((page) => (
           <Link to={page.path}>{page.name}</Link>
         ))}
+      </div>
       </div>
     </div>
   )
