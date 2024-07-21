3badf28a85c710943c53940aa0d44e079fab7209
zucm709 <zuckm709@gmail.com>
2024-07-21 16:47:03 +0100

    quick sort

--- library/sort_algorithm/quick_sort.js
+++ library/sort_algorithm/quick_sort.js
@@ -1,26 +1,37 @@
-function pivot(array,start,end){
-    function swap(arr,i,j){
-        let temp = arr[i]
-        arr[i] = array[j]
-        arr[j] = temp;
-    }
-    start = 0;
-    end = array.length -1;
-    let pivot = array[start];
+function pivot(array, start = 0, end = array.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+    };
+    // function swap(arr, idx1, idx2) {
+    //     let temp = arr[idx1]
+    //     arr[idx1] = arr[idx2]
+    //     arr[idx2] = temp
+    // }
+
+    let pivotal = array[start];
     let swapidx = start
-   for(let i = start + 1; i < array.length; i++){
-        if(pivot > array[i]){
+    for (let i = start + 1; i <= end; i++) {
+        if (pivotal > array[i]) {
             swapidx++;
-            swap(array, swapidx,i)
+            swap(array, swapidx, i)
+            // console.log(array)
         }
-   }
-   swap(array,start,swapidx)
-   console.log(array)
-   return array;
+    }
+    swap(array, start, swapidx)
+    return swapidx;
 }
-const src = pivot([13,73,43,33,53,23,63])
+const src = pivot([5, 8, 1, 3, 6, 2, 4, 7])
 console.log(src)
 
-function quickSort(file){
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right)
 
+        quickSort(arr, left, pivotIndex - 1)
+
+        quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr;
 }
+const dir = quickSort([5, 8, 1, 3, 6, 2, 4, 7])
+console.log(dir)
